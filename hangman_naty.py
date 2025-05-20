af3e9f365a8e9cdbdc09842b5c67455c5df454ce
Natalie <nbozic@tsn.at>
2025-05-20 10:11:58 +0200

    displayed hangman

--- hangman_naty.py
+++ hangman_naty.py
@@ -1,12 +1,117 @@
-import customtkinter
+import random
 
-def button_callback():
-    print("button clicked")
+def guess_word():
+    words = ["python", "hangman", "challenge", "programming", "computer"]
+    return random.choice(words)
 
-app = customtkinter.CTk()
-app.geometry("400x150")
+def display_hangman(tries):
+    stages = [
+        """
+           ------
+           |    |
+           |    O
+           |   /|\\
+           |   / \\
+           |
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |   /|\\
+           |   /
+           |
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |   /|
+           |
+           |
+        """,
+        """
+           ------
+           |    |
+           |    O
+           |    
+           |
+           |
+        """,
+        """
+           ------
+           |    |
+           |    
+           |
+           |
+        """,
+        """
+           ------
+           |    
+           |    
+           |
+           |
+        """,
+        """
+           
+           
+           
+           
+           
+        """,
+    ]
+    return stages[tries]
 
-button = customtkinter.CTkButton(app, text="Start", command=button_callback)
-button.pack(padx=20, pady=20)
+def play_hangman():
+    word = guess_word()
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    word_completion = "_" * len(word)
+    print("Let's play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
 
-app.mainloop()
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word: ").lower()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word.")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Invalid input. Please try again.")
+
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n") 
+    if guessed:
+        print("Congratulations! You guessed the word", word, "!")
+    else:
+        print("Sorry, you ran out of tries. The word was", word, ". Maybe next time!")
+if __name__ == "__main__":
+    play_hangman()
